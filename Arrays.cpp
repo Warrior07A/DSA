0a77ac5825f24179a4c42ebbeb370a8d4ea5aa54
Warrior07A <akshatmittalofficial@gmail.com>
2025-06-09 13:55:41 +0000

    CLASS NOTES

--- Arrays.cpp
+++ Arrays.cpp
@@ -7,27 +7,9 @@ int S[100];   //CREATING AN ARRAY TO STORE 100 VARIABLES IN MEMORY
 
 S[5]=10; //setting value at index 5=10
 
-[10,"abc",true]//VALID IN JS
+[10,"abc",true]//VALID IN JS~~~INVALID IN CPP
 
 [1,2,4,4,5,] //valid in cpp(type =samee)
 
-//(A)  SUMMATION
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-  long long N,sum=0; //since second line goes to 10**9
-  cin>>N;
-  int arr[N];
-  for (int i=0;i<N;i++){
-    cin>>arr[i];
-  }
-  for (int j=0;j<N;j++){
-    sum+=arr[j];
-  }
-  cout<<abs(sum);
-}
-
-//(B) SEARCHING
 
 
